7a9815fe56479a96c47c1df6ead6932fe0bfb493
jay <jay@gmail.com>
2024-09-26 10:05:11 +0300

    update,delete

--- app.py
+++ app.py
@@ -31,6 +31,33 @@ def index():
     else:
         todos=Todo.query.order_by("created_at").all()
         return render_template("index.html",todos=todos)
+    
+@app.route('/delete/<Id>')
+def delete(Id):
+    todo=Todo.query.get_or_404(Id)
+    
+    try:
+        db.session.delete(todo)
+        db.session.commit()
+
+        return redirect('/')  
+    except:
+        return "Thre was an error deleting the item"  
+
+@app.route('/update/<Id>',methods=["GET","POST"])
+def update(Id):
+    todo=Todo.query.get_or_404(Id)
+    if request.method=="POST":
+        new_todo_content=request.form['content']
+        todo.content=new_todo_content
+
+        db.session.commit()
+
+        return redirect("/")
+    else:
+        return render_template("update.html",todo=todo)
+
+
 
 
  #Function to initialize the database if it doesn't exist
